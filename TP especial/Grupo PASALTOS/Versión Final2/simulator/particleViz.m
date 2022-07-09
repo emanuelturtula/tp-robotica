a1283260a5b9b33968730933a9e87c4ac4a08b03
Emanuel Turtula <emanuel.turtula@mercadolibre.com>
2022-07-09 12:56:32 -0300

    Add tp matilde

---
+++ TP especial/Grupo PASALTOS/Versión Final2/simulator/particleViz.m
@@ -0,0 +1,10 @@
+classdef particleViz
+    %% PROPERTIES
+    properties
+        handle_est;
+        handle_particles;
+        handle_pose;
+        handle_lidar;
+        handle_robot;
+    end
+end
